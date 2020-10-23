81b9e6ea2f74d7116551db503f2a58d746f7f598
watislaf <vladkozulin@mail.ru>
2020-10-24 00:43:24 +0300

    folder rename

--- app-data/build/resources/main/data.sql
+++
@@ -1,3 +0,0 @@
-delete from Task ;
-insert into Task (id, name, string, number, created_at)
-values ('0', 'easy', 'go to gum', 42, '26-12-2001');
--- app-data/build/resources/main/schema.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists Task
-(
-    id         serial primary key,
-    name       varchar(50)   not null,
-    string     varchar(1000) not null,
-    number     integer          not null,
-    created_at timestamp     not null
-);
